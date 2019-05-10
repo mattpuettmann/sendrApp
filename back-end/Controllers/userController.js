84b8fe40c8a3154d28cdd7ca2bfb596199190e0b
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-05-10 15:52:21 -0600

    user controller taking shape unless im just misunderstanding postman

--- back-end/Controllers/userController.js
+++ back-end/Controllers/userController.js
@@ -1,9 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../Models/user');
 
-router.get('/', (req, res) => {
-    res.send('ayo');
-})
+
+
+router.get('/', async (req, res, next) => {
+    console.log('this is get all current users')
+       try  {  
+        const allUsers = await User.find();
+        res.json({
+          status: 200,
+          data: allUsers
+        }); 
+      } catch(err){ 
+        res.send(err)  
+      }
+  });
+
+
+  router.post('/', async (req, res) => {
+    try {
+      console.log('new user created');
+      const createdUser = await User.create(req.body);
+      console.log('response happening?')
+      res.json({
+        status: 200,
+        data: createdUser
+      });
+    } catch(err){
+      console.log(err);
+      res.send(err);
+    }
+  });
+
+
+  router.get('/:id', async (req, res, next) => {
+    try  {
+       const foundUser = await User.findById(req.params.id);
+       res.json({
+         status: 200,
+         data: foundUser
+       });
+     } catch (err){
+       res.send(err);
+     }
+});
+
+
+router.put('/:id', async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true});
+      res.json({
+        status: 200,
+        data: updatedUser
+      });
+    } catch(err){
+      res.send(err)
+    }
+  });
 
 
 
