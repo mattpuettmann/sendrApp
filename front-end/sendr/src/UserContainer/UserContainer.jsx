a86385ec9b1a6893b49a55890475d26d98edb95e
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-05-13 10:29:21 -0600

    React initialized

---
+++ front-end/sendr/src/UserContainer/UserContainer.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class UserContainer extends Component {
+    constructor(){
+        super();
+        
+    }
+    render(){
+        return <div>
+            <h3>SENDR User Container</h3>
+        </div>
+    }
+}
+
+export default UserContainer;
